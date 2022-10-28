a49d1c99c138797d640b66233ac813602cd3db33
SedoyDi <95245699+SedoyDi@users.noreply.github.com>
2022-10-29 00:54:04 +0300

    feet: .env

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,6 +1,8 @@
 const jwt = require('jsonwebtoken');
 const AuthError = require('../errors/authError');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
@@ -10,7 +12,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, 'some-secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
   } catch (err) {
     throw new AuthError('Необходима авторизация');
   }
